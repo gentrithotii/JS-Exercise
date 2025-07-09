8826d67618ac49d4ab5ee7cd589013457373644b
Gentrithotii <Gentrithoti@hotmail.com>
2025-07-09 22:46:12 +0200

    More fixes

--- exercise.js
+++ exercise.js
@@ -12,15 +12,16 @@ function displayCurrentDate() {
 }
 
 function checkIfLeapYear() {
+  event.preventDefault();
   let num = parseInt(document.getElementById("check-leap").value);
+  // console.log("Entered number: " + num);
 
-  console.log("Entered number: " + num);
-
-  let isLeap = "Is not Leap year";
+  let isLeap = "It's not leap year";
 
   if ((num % 4 === 0 && num % 100 !== 0) || num % 400 === 0) {
     isLeap = "Is Leap year";
   }
+
   document.getElementById("leap-boolean").innerHTML = isLeap;
 
   return false;
@@ -39,11 +40,11 @@ function checkIfSundayBetweenYears() {
   const numberOfYears = 2050 - 2014;
   const date = new Date(2014, 0, 1);
   for (let i = 1; i <= numberOfYears; i++) {
-    console.log(date.getMonth(), date.getDate());
+    // console.log(date.getMonth(), date.getDate());
 
     date.setFullYear(date.getFullYear() + 1);
     if (date.getMonth() === 0 && days[date.getDay()] === "Sunday") {
-      console.log("Date between: " + date);
+      // console.log("Date between: " + date);
       document.getElementById("date-between").innerHTML = date;
     }
   }
